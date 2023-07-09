14dc7c354f6d405be42422eca12d5f58d72d8467
Hrnjez <h.nemanja@yahoo.com>
2023-07-09 14:50:08 +0200

    setup project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
+        <Home />
       </div>
     </div>
   );
