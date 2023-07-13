0ece4e160d98faa0754d79bdada93a535e6dc8a9
Hrnjez <h.nemanja@yahoo.com>
2023-07-13 22:51:38 +0200

    create custom hooks to fetch the data

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,17 +1,15 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
-
-    return ( 
-        <div className="blog-list">
-            <h2>{ title }</h2>
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
-                </div>
-            ))}
-        </div>
-     );
+const BlogList = ({ blogs, title }) => {
+	return (
+		<div className="blog-list">
+			<h2>{title}</h2>
+			{blogs.map((blog) => (
+				<div className="blog-preview" key={blog.id}>
+					<h2>{blog.title}</h2>
+					<p>Written by {blog.author}</p>
+				</div>
+			))}
+		</div>
+	);
 }
- 
+
 export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,16 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
-const Home = () => {    
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
 
-    const [name, setName] = useState("mario")
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
-    useEffect(() => {
-        console.log('use effect ran');
-        console.log(name);
-    }, [name]); 
-    // CASE 1: '[]'-> empty square brackets here means it only runs once at first render
-    // CASE 2: '[name]' -> it runs everytime when that 'name' state changes
-    
-    return ( 
-        <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <button onClick={() => setName('luigi')}>change name </button>
-            <p>{name}</p>
-        </div>
-     );
+const Home = () => {
+	const { data, isPending, error } = useFetch('http://localhost:8000/blogs');
+	return (
+		<div className="home">
+			{ error && <div>{ error }</div>}
+			{isPending && <div>Loading...</div>}
+			{data && <BlogList blogs={data} title="All Blogs" />}
+		</div>
+	);
 }
- 
+
 export default Home;
